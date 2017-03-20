0a30bb03dd696ce99c99bed9b63c1b8ee3fbf1f1
atiartalukdar <swajannsu445>
2017-03-20 22:00:07 +0600

    initial + pz_lvl1

--- app/src/main/java/info/atiar/pnotca/puzzleL1/PZ_L1_G1.java
+++ app/src/main/java/info/atiar/pnotca/puzzleL1/PZ_L1_G1.java
@@ -6,13 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.DragEvent;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import info.atiar.pnotca.R;
 
 public class PZ_L1_G1 extends AppCompatActivity {
-    ImageButton target1,target2,target3,target4,source1,source2,source3,source4;
+    ImageView target1,target2,target3,target4,source1,source2,source3,source4;
     LinearLayout l1,l2,l3,l4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,36 +23,18 @@ public class PZ_L1_G1 extends AppCompatActivity {
 
         setContentView(R.layout.activity_pz__l1__g1);
 
-        target1 = (ImageButton) findViewById(R.id.pz_l1_a1);
-        target2 = (ImageButton) findViewById(R.id.pz_l1_a2);
-        target3 = (ImageButton) findViewById(R.id.pz_l1_a3);
-        target4 = (ImageButton) findViewById(R.id.pz_l1_a4);
+        target1 = (ImageView) findViewById(R.id.pz_l1_11);
+        target2 = (ImageView) findViewById(R.id.pz_l1_12);
 
-        l1 = (LinearLayout) findViewById(R.id.a);
-        l2 = (LinearLayout) findViewById(R.id.b);
-        l3 = (LinearLayout) findViewById(R.id.c);
-        l4 = (LinearLayout) findViewById(R.id.d);
-
-        source1 = (ImageButton) findViewById(R.id.pz_l1_qa);
-        source2 = (ImageButton) findViewById(R.id.pz_l1_qb);
-        source3 = (ImageButton) findViewById(R.id.pz_l1_qc);
-        source4 = (ImageButton) findViewById(R.id.pz_l1_qd);
+        source1 = (ImageView) findViewById(R.id.pz_l1_q_11);
+        source2 = (ImageView) findViewById(R.id.pz_l1_q_12);
 
 
         target1.setOnDragListener(dragListener);
         target2.setOnDragListener(dragListener);
-        target3.setOnDragListener(dragListener);
-        target4.setOnDragListener(dragListener);
-
-        l1.setOnDragListener(dragListener);
-        l2.setOnDragListener(dragListener);
-        l3.setOnDragListener(dragListener);
-        l4.setOnDragListener(dragListener);
 
         source1.setOnLongClickListener(longClickListener);
         source2.setOnLongClickListener(longClickListener);
-        source3.setOnLongClickListener(longClickListener);
-        source4.setOnLongClickListener(longClickListener);
 
     }
 
@@ -79,15 +61,24 @@ public class PZ_L1_G1 extends AppCompatActivity {
                 case DragEvent.ACTION_DRAG_EXITED:
                     break;
                 case DragEvent.ACTION_DROP:
-                    if (view.getId() == R.id.pz_l1_qa && view.getId() == R.id.a){
-                        ((ImageButton) view).setImageResource(R.drawable.pz_l1_qa);
-                    }
+                    view.animate()
+                            .x(target1.getX())
+                            .y(target1.getY())
+                            .setDuration(1000)
+                            .start();
 
-                        view.animate()
-                                .x(l1.getX())
-                                .y(l2.getY())
-                                .setDuration(1000)
-                                .start();
+                    /*if (view.getId() == R.id.pz_l1_q_11 && view.getId() == R.id.pz_l1_11){
+                        ((ImageView) view).setImageResource(R.drawable.a);
+
+                        Toast.makeText(PZ_L1_G1.this, "Image dropped 1",Toast.LENGTH_LONG).show();
+                    }
+                    else if (view.getId() == R.id.pz_l1_q_11 && view.getId() == R.id.pz_l1_11){
+                        ((ImageView) view).setImageResource(R.drawable.b);
+
+                        Toast.makeText(PZ_L1_G1.this, "Image dropped 2",Toast.LENGTH_LONG).show();
+                    }*/
+
+
 
 
                     break;
